28d12a95d4b058d2f6fca5317cc03ba9e4a9f20d
李健 <li.j@uuzu.com>
2019-03-18 12:10:46 +0800

    上传项目

---
+++ MobPushDemoSwift/UI/SelectModel.swift
@@ -0,0 +1,17 @@
+//
+//  SelectModel.swift
+//  MobPushDemoSwift
+//
+//  Created by LeeJay on 2019/1/24.
+//  Copyright © 2019 YouZu. All rights reserved.
+//
+
+import UIKit
+
+class SelectModel: NSObject {
+
+    var title: String?
+    var selected = false
+    var selectedColor: UIColor?
+    var value: NSNumber?
+}
---
+++ MobPushDemoSwift/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  MobPushDemoSwift
+//
+//  Created by LeeJay on 2019/1/23.
+//  Copyright © 2019 YouZu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
