889d82a5e3fc057fe10790531080a3bac3e5cfc7
李健 <li.j@uuzu.com>
2019-03-18 12:09:59 +0800

    删除项目

--- MobPushDemoSwift/MobPushDemoSwift-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <MobPush/MobPush.h>
-#import <MobFoundation/MobFoundation.h>
-#import <IQKeyboardManager/IQTextView.h>
-#import <MobPush/MobPush+Test.h>
-#import <MobPush/UIViewController+MobPush.h>
